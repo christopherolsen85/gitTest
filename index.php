7de0cc33d578bdc1909afb134f7132a73cda42e1
christopherolsen85 <christopherolsen85@yahoo.com>
2015-11-19 12:49:26 -0600

    Added basic routing framework

--- index.php
+++ index.php
@@ -1,5 +1,43 @@
 <?php
 
-echo "testing";
+$requestUri = getCurrentUri();
+$routes = getRoutes($requestUri);
+print_r($routes);
+
+if(count($routes) < 1)
+{
+	echo "home page!";
+	
+}
+
+else if(isset($routes[0]))
+{
+	echo "testing";
+}
+
+
+//get the current uri
+function getCurrentUri()
+{
+	$basepath = implode('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1)) . '/';
+	$uri = substr($_SERVER['REQUEST_URI'], strlen($basepath));
+	if (strstr($uri, '?')) $uri = substr($uri, 0, strpos($uri, '?'));
+	$uri = '/' . trim($uri, '/');
+	return $uri;
+}
+
+//based on a uri passed in as a parameter, return an array of the routes contained in that uri 
+function getRoutes($uri)
+{
+	$routes = Array();
+	$routes = explode("/", $uri);
+	$finalRoutes = Array();
+	foreach($routes as $route)
+	{
+		if($route != "")
+			$finalRoutes[] = $route;
+	}
+	return $finalRoutes;
+}
 
 ?>
